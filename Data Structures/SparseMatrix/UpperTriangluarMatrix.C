9b2f8bf2585b8b59effe7d094d752c3ac157211a
JAGnotoes <abhimanyusingh.0247@gmail.com>
2025-03-20 14:03:22 +0530

    Added SparseMatrix: LoweTriangularMatrix, UpperTriangularMatrix

---
+++ Data Structures/SparseMatrix/UpperTriangluarMatrix.C
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    system("cls");
+    int arr[4][4];
+    int count = 1;
+
+    printf("Start Entering elements it's 4x4 matrix so you'll be entering 10 elements: \n");
+    for (int i = 0; i < 4; i++)
+    {
+        for (int j = 0; j < 4; j++)
+        {
+            if (i > j)
+            {
+                arr[i][j] = 0;
+            }
+            else
+            {
+                printf("%d: ", count);
+                scanf("%d", &arr[i][j]);
+                count++;
+            }
+        }
+    }
+
+    for (int i = 0; i < 4; i++)
+    {
+        for (int j = 0; j < 4; j++)
+        {
+            printf(" %d\t", arr[i][j]);
+        }
+        printf("\n");
+    }
+}
